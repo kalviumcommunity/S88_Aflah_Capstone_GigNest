8ae65c8646fd60ac4000491b34ef823c131e3283
mhdaflah <muhammed.aflah.s88@kalvium.community>
2025-05-15 12:01:31 +0530

    backend created

--- backend/server.js
+++ backend/server.js
@@ -14,8 +14,6 @@ app.use(express.json());
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
     });
     console.log('✅ MongoDB connected successfully');
   } catch (err) {
